87f7df31b5f2b4180548069bbd994227bf26e462
dmitrysuraho <dmitriysurago@mail.ru>
2021-10-10 20:18:46 +0300

    adding docker support

--- WebApplication/ClientApp/src/environments/environment.prod.ts
+++ WebApplication/ClientApp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-    production: true
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyByOfZ8XxYUBqOUtxuEvHxkkkfEcB28TU0",
+        authDomain: "dating-agency-3088f.firebaseapp.com",
+        projectId: "dating-agency-3088f",
+        storageBucket: "dating-agency-3088f.appspot.com",
+        messagingSenderId: "811604666524",
+        appId: "1:811604666524:web:a7140486cee443b3f42559",
+        measurementId: "G-Q58MBZ8D66"
+    }
 };
