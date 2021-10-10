5852b94f51f55ce953d51cd78e66f546764586c4
dmitrysuraho <dmitriysurago@mail.ru>
2021-10-10 12:39:12 +0300

    changing

--- WebApplication/ClientApp/src/app/layout/layouts/vertical/compact/compact.module.ts
+++ WebApplication/ClientApp/src/app/layout/layouts/vertical/compact/compact.module.ts
@@ -10,9 +10,7 @@ import { FuseNavigationModule } from '@fuse/components/navigation';
 import { LanguagesModule } from 'app/layout/common/languages/languages.module';
 import { MessagesModule } from 'app/layout/common/messages/messages.module';
 import { NotificationsModule } from 'app/layout/common/notifications/notifications.module';
-import { QuickChatModule } from 'app/layout/common/quick-chat/quick-chat.module';
 import { SearchModule } from 'app/layout/common/search/search.module';
-import { ShortcutsModule } from 'app/layout/common/shortcuts/shortcuts.module';
 import { UserModule } from 'app/layout/common/user/user.module';
 import { SharedModule } from 'app/shared/shared.module';
 import { CompactLayoutComponent } from 'app/layout/layouts/vertical/compact/compact.component';
@@ -33,9 +31,7 @@ import { CompactLayoutComponent } from 'app/layout/layouts/vertical/compact/comp
         LanguagesModule,
         MessagesModule,
         NotificationsModule,
-        QuickChatModule,
         SearchModule,
-        ShortcutsModule,
         UserModule,
         SharedModule
     ],
