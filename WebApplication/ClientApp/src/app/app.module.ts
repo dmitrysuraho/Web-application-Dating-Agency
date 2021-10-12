4557048aaddf3c0a823b41973a56f2ad0a83f593
dmitrysuraho <dmitriysurago@mail.ru>
2021-10-12 14:05:19 +0300

    deleting i18n

--- WebApplication/ClientApp/src/app/app.module.ts
+++ WebApplication/ClientApp/src/app/app.module.ts
@@ -6,14 +6,14 @@ import { MarkdownModule } from 'ngx-markdown';
 import { FuseModule } from '@fuse';
 import { FuseConfigModule } from '@fuse/services/config';
 import { FuseMockApiModule } from '@fuse/lib/mock-api';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
 import { CoreModule } from 'app/core/core.module';
 import { appConfig } from 'app/core/config/app.config';
 import { mockApiServices } from 'app/mock-api';
 import { LayoutModule } from 'app/layout/layout.module';
 import { AppComponent } from 'app/app.component';
 import { appRoutes } from 'app/app.routing';
-import { AngularFireModule } from "@angular/fire/compat";
-import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
 import { environment } from "../environments/environment";
 
 const routerConfig: ExtraOptions = {
--- WebApplication/ClientApp/src/app/layout/common/languages/languages.component.ts
+++ WebApplication/ClientApp/src/app/layout/common/languages/languages.component.ts
@@ -1,7 +1,4 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { take } from 'rxjs/operators';
-import { AvailableLangs, TranslocoService } from '@ngneat/transloco';
-import { FuseNavigationService, FuseVerticalNavigationComponent } from '@fuse/components/navigation';
 
 @Component({
     selector       : 'languages',
@@ -12,7 +9,7 @@ import { FuseNavigationService, FuseVerticalNavigationComponent } from '@fuse/co
 })
 export class LanguagesComponent implements OnInit, OnDestroy
 {
-    availableLangs: AvailableLangs;
+    availableLangs: any[] = [];
     activeLang: string;
     flagCodes: any;
 
@@ -20,9 +17,7 @@ export class LanguagesComponent implements OnInit, OnDestroy
      * Constructor
      */
     constructor(
-        private _changeDetectorRef: ChangeDetectorRef,
-        private _fuseNavigationService: FuseNavigationService,
-        private _translocoService: TranslocoService
+        private _changeDetectorRef: ChangeDetectorRef
     )
     {
     }
@@ -36,18 +31,6 @@ export class LanguagesComponent implements OnInit, OnDestroy
      */
     ngOnInit(): void
     {
-        // Get the available languages from transloco
-        this.availableLangs = this._translocoService.getAvailableLangs();
-
-        // Subscribe to language changes
-        this._translocoService.langChanges$.subscribe((activeLang) => {
-
-            // Get the active lang
-            this.activeLang = activeLang;
-
-            // Update the navigation
-            this._updateNavigation(activeLang);
-        });
 
         // Set the country iso codes for languages for flags
         this.flagCodes = {
@@ -74,8 +57,6 @@ export class LanguagesComponent implements OnInit, OnDestroy
      */
     setActiveLang(lang: string): void
     {
-        // Set the active lang
-        this._translocoService.setActiveLang(lang);
     }
 
     /**
@@ -93,61 +74,5 @@ export class LanguagesComponent implements OnInit, OnDestroy
     // @ Private methods
     // -----------------------------------------------------------------------------------------------------
 
-    /**
-     * Update the navigation
-     *
-     * @param lang
-     * @private
-     */
-    private _updateNavigation(lang: string): void
-    {
-        // For the demonstration purposes, we will only update the Dashboard names
-        // from the navigation but you can do a full swap and change the entire
-        // navigation data.
-        //
-        // You can import the data from a file or request it from your backend,
-        // it's up to you.
 
-        // Get the component -> navigation data -> item
-        const navComponent = this._fuseNavigationService.getComponent<FuseVerticalNavigationComponent>('mainNavigation');
-
-        // Return if the navigation component does not exist
-        if ( !navComponent )
-        {
-            return null;
-        }
-
-        // Get the flat navigation data
-        const navigation = navComponent.navigation;
-
-        // Get the Project dashboard item and update its title
-        const projectDashboardItem = this._fuseNavigationService.getItem('dashboards.project', navigation);
-        if ( projectDashboardItem )
-        {
-            this._translocoService.selectTranslate('Project').pipe(take(1))
-                .subscribe((translation) => {
-
-                    // Set the title
-                    projectDashboardItem.title = translation;
-
-                    // Refresh the navigation component
-                    navComponent.refresh();
-                });
-        }
-
-        // Get the Analytics dashboard item and update its title
-        const analyticsDashboardItem = this._fuseNavigationService.getItem('dashboards.analytics', navigation);
-        if ( analyticsDashboardItem )
-        {
-            this._translocoService.selectTranslate('Analytics').pipe(take(1))
-                .subscribe((translation) => {
-
-                    // Set the title
-                    analyticsDashboardItem.title = translation;
-
-                    // Refresh the navigation component
-                    navComponent.refresh();
-                });
-        }
-    }
 }
